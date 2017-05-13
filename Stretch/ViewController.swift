6e4b2e2d284307bdfc636d91d8df010d76ba523a
forrestsyrett <tlpaadmin@TLPAAdmins-MacBook-Pro.local>
2017-05-13 13:42:38 -0600

    Initial Commit

---
+++ Stretch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stretch
+//
+//  Created by TLPAAdmin on 5/13/17.
+//  Copyright © 2017 Forrest Syrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
